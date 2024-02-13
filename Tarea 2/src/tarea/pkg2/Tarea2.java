da57b5b4de8271f1397c40739d12c3d218a6cb75
harukixzh <bryan2014ap@gmail.com>
2024-02-12 19:39:59 -0600

    Mas operaciones

--- Tarea 2/src/tarea/pkg2/Tarea2.java
+++ Tarea 2/src/tarea/pkg2/Tarea2.java
@@ -28,7 +28,31 @@ public class Tarea2 {
             opcion = scanner.nextInt();
             
             if (opcion == 1) {
-                System.out.println("Por favor colocal los números que desea sumar");
+                
+                suma(scanner);
+            }
+            
+            if (opcion == 2) {
+                
+                resta(scanner);
+            }
+            
+            if (opcion == 3) {
+                
+                multiplicacion(scanner);
+                
+            }
+            
+            if (opcion == 4) {
+                
+                division(scanner);
+                
+            }
+        } while (opcion !=4);
+        
+    }
+    public static void suma(Scanner scanner){
+        System.out.println("Por favor colocar dos numeros que desea sumar");
                 System.out.println("");
                 int a = 0;
                 int b = 0;
@@ -42,9 +66,70 @@ public class Tarea2 {
                 System.out.println(c);
                 System.out.println("");
                 
-            }
-        } while (opcion !=4);
+    }
+    public static void resta(Scanner scanner){
+        int a=0;
+        int b=0;
+        int c=0;
+        
+        System.out.println("Coloque dos numeros que desee restar: ");
+        System.out.println("");
+        System.out.println("Coloque el minuendo");
+        System.out.println("");
+        a = scanner.nextInt();
+        System.out.println("");
+        System.out.println("Coloque el sustraendo");
+        System.out.println("");
+        b = scanner.nextInt();
+        System.out.println("");
+        System.out.println("La diferencia es: ");
+        c = a-b;
+        System.out.println(c);
+        System.out.println("");
+    }
+    
+    public static void multiplicacion(Scanner scanner){
+        int a=0;
+        int b=0;
+        int c=0;
+        
+        System.out.println("Coloque dos numeros que desee multiplicar: ");
+        System.out.println("");
+        a = scanner.nextInt();
+        System.out.println("");
+        b = scanner.nextInt();
+        System.out.println("");
+        c = a*b;
+        System.out.print("La multiplicacion de " + a + "*" + b + " es: ");
+        System.out.println(c);
+        System.out.println("");
         
     }
+    public static void division(Scanner scanner){
+        System.out.println("Coloque dos números que desee dividir");
+        System.out.println("");
+        int numerador = 1;
+        int denominador = 1;
+        int resultado = 0;
+        System.out.println("Coloque el numerador: ");
+        numerador = scanner.nextInt();
+        System.out.println("");
+        System.out.println("Coloque el denominador");
+        denominador = scanner.nextInt();
+        System.out.println("");
+        if (denominador != 0){
+            resultado = numerador / denominador;
+            System.out.println("La division de " + numerador + "/" + denominador + "es: ");
+            System.out.println("");
+            System.out.println(resultado);
+        }
+        else{
+            System.out.println("No se puede dividir dentro de cero, vuelva a ingresar datos: ");
+            System.out.println("");
+        }
+        
+            
+        
+}
     
 }
